18d22384b8a0ed32c13800247ba2840c272a5426
Mohamed Elhaj <mo.elhaj@outlook.com>
2024-10-22 17:40:48 +0400

    build fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import fs from "fs";
-import path from "path";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -26,36 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//serving files for authenticated user
-app.get("/file/:filename", auth as express.RequestHandler, (req, res) => {
-	const filename = req.params.filename;
-	const filepath = path.join(__dirname, "/src/" + "/../../uploads/", filename);
-
-	// Check if the file exists
-	fs.access(filepath, fs.constants.F_OK, err => {
-		if (err) {
-			return res.status(404).send("File not found");
-		}
-
-		// Send the file to the authenticated user
-		res.sendFile(filepath);
-	});
-});
-
 app.use("/api/v1", routes);
-
-// serving the frontend
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
-
-	app.get("*", (req: Request, res: Response) =>
-		res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"))
-	);
-} else {
-	app.get("/", (req: Request, res: Response) => {
-		res.send("Please set to production");
-	});
-}
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 	console.log(err);
